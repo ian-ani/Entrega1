895937188a6717df00b9e50cbb91124f44575bbb
Iris <irisaunav@gmail.com>
2026-03-18 12:23:49 +0100

    Crear clase EmpleadoBR

---
+++ src/es/iesmz/EmpleadoBR.java
@@ -0,0 +1,13 @@
+package es.iesmz;
+
+public class EmpleadoBR {
+    /* OTROS METODOS */
+    
+    public static float calculaSalarioBruto(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+        return 0.0f;
+    }
+
+    public static float calculaSalarioNeto(float salarioBruto) {
+        return 0.0f;
+    }
+}
