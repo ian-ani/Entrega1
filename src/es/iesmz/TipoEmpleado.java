8975b0c2ae91a51e54a403e975641916a7073ae2
Iris <irisaunav@gmail.com>
2026-03-18 16:39:44 +0100

    Añadir método isMember al enum TipoEmpleado

--- src/es/iesmz/TipoEmpleado.java
+++ src/es/iesmz/TipoEmpleado.java
@@ -1,7 +1,17 @@
 package es.iesmz;
 
+import java.util.List;
+
 public enum TipoEmpleado {
     vendedor, encargado;
 
-    // TODO crear el metodo isMember
+    static boolean isMember(String valor) {
+        List<TipoEmpleado> tipos = List.of(TipoEmpleado.values());
+
+        for (TipoEmpleado tipo: tipos) {
+            if (tipo.name().equalsIgnoreCase(valor)) return true;
+        }
+
+        return false;
+    }
 }
